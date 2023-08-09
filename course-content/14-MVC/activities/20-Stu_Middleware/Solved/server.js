f4b04ffb0085ea06768517b6f53973011ce3dbe4
unknown <lukeharriman1013@gmail.com>
2023-08-09 09:55:06 -0700

    update

---
+++ course-content/14-MVC/activities/20-Stu_Middleware/Solved/server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  //https://www.npmjs.com/package/express-session#resave
+  //Forces the session to be saved back to the session store, even if the session was never modified during the request.
+  resave: false,
+  //https://www.npmjs.com/package/express-session#saveuninitialized
+  //Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified.
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
